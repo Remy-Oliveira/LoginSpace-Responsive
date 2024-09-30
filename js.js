2bfd6541795624bfc291b214688a77bfffc7a6e7
Remy-Oliveira <remy.musico@gmail.com>
2024-09-29 21:02:55 -0300

    Created

---
+++ js.js
@@ -0,0 +1,7 @@
+function hamburguer() {
+    document.querySelector(".wrapper").classList.toggle("hamburguer");
+}
+
+function cadastro() {
+    document.querySelector(".wrapper").classList.toggle("cadastro");
+}
